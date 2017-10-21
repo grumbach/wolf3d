bb0349e7b41d375cbf88ae3a675a56bfc0e48df0
grumbach <agrumbac@student.42.fr>
2017-10-21 23:11:05 +0200

    [CLEAN] cleaned and optimised

--- srcs/C/wolf3d.c
+++ srcs/C/wolf3d.c
@@ -6,7 +6,7 @@
 /*   By: agrumbac <agrumbac@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/06/18 23:52:41 by agrumbac          #+#    #+#             */
-/*   Updated: 2017/10/21 20:05:13 by agrumbac         ###   ########.fr       */
+/*   Updated: 2017/10/21 22:52:55 by agrumbac         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,50 +16,39 @@
 ** cl_run has 4 args
 **     - [r--] the map
 **     - [r--] the camera
-**     - [-w-] wall_heights (written in the first line of pixeltab, for memory efficiency)
-**     - [-w-] wall_colors (written in the middle of pixeltab, as they won't need to change color...)
-*/
-
-static void	put_pixel(t_sdl *sdl, const t_yx i, uint color)
-{
-	sdl->pixels[sdl->size.x * i.y + i.x] = color;// TODO change this to sexy pointer
-}
-
-/*
-** -----------------------------------------  win size -> UP
-**       -----------------------------        wallheight
-** ++++++                             ++++++  diff
-** ======                                     GROUND
-**       =============================        WALL
-** 	                                  ======  SKYBOX
+**     - [-w-] wall_heights
+**       (written in the first line of pixeltab, for memory efficiency)
+**     - [-w-] wall_colors
+**       (written in the middle of pixeltab, they won't need to change color..)
 */
 
 static void	map_redraw(t_sdl *sdl)
 {
+	uint32_t	(*pixels)[sdl->size.y][sdl->size.x];
 	t_yx		i;
 	uint		wall_color;
 	int			height;
 	int			step;
 
+	pixels = (void*)sdl->pixels;
 	i = (t_yx){sdl->size.y, sdl->size.x};
 	while (i.x--)
 	{
 		i.y = sdl->size.y;
-		wall_color = *(sdl->pixels + (((sdl->size.y % 2 ? \
-			sdl->size.y - 1 : sdl->size.y) / 2) * sdl->size.x));
-		height = sdl->pixels[i.x];
-		if (height > sdl->size.y) {ft_printf("OUT OF BOUNDS height TOO HIGH!!"); break;}//
-
+		wall_color = (*pixels)\
+			[((sdl->size.y % 2 ? sdl->size.y - 1 : sdl->size.y) / 2)][i.x];
+		height = (*pixels)[0][i.x];
+		if (height > sdl->size.y) {ft_printf("OUT OF BOUNDS height TOO HIGH!!"); break;}//rm line
 		step = sdl->size.y - ((sdl->size.y - height) / 2);
 		while (--i.y > step)
-			put_pixel(sdl, i, GROUND);
+			(*pixels)[i.y][i.x] = GROUND;
 		step = sdl->size.y - height - ((sdl->size.y - height) / 2);
 		i.y++;
 		while (--i.y > step)
-			put_pixel(sdl, i, wall_color);
+			(*pixels)[i.y][i.x] = wall_color;
 		i.y++;
 		while (--i.y >= 0)
-			put_pixel(sdl, i, SKYBOX);
+			(*pixels)[i.y][i.x] = SKYBOX;
 	}
 }
 
@@ -76,7 +65,7 @@ static void	main_loop(t_cl *cl, t_sdl *sdl)
 		{
 			cl_run(cl, (size_t[WORK_DIM]){sdl->size.x}, 3, \
 				(t_arg){&cam, sizeof(t_cam), CL_MEM_READ_ONLY}, \
-				(t_arg){sdl->pixels, sizeof(uint) * sdl->size.x, CL_MEM_WRITE_ONLY}, \
+				(t_arg){sdl->pixels, 4 * sdl->size.x, CL_MEM_WRITE_ONLY}, \
 				(t_arg){(sdl->pixels + (((sdl->size.y % 2 ? \
 					sdl->size.y - 1 : sdl->size.y) / 2) * sdl->size.x)), \
 					sizeof(uint) * sdl->size.x, CL_MEM_WRITE_ONLY});
@@ -87,42 +76,45 @@ static void	main_loop(t_cl *cl, t_sdl *sdl)
 	}
 }
 
+static void	map_init(char map[MAP_H][MAP_W])
+{
+	map = (char [MAP_H][MAP_W]){
+	{1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+	{1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+	{1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+	{1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+	{1, 0, 0, 0, 0, 0, 2, 2, 2, 2, 2, 0, 0, 0, 0, 3, 0, 3, 0, 3, 0, 0, 0, 1},
+	{1, 0, 0, 0, 0, 0, 2, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+	{1, 0, 0, 0, 0, 0, 2, 0, 0, 0, 2, 0, 0, 0, 0, 3, 0, 0, 0, 3, 0, 0, 0, 1},
+	{1, 0, 0, 0, 0, 0, 2, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+	{1, 0, 0, 0, 0, 0, 2, 2, 0, 2, 2, 0, 0, 0, 0, 3, 0, 3, 0, 3, 0, 0, 0, 1},
+	{1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+	{1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+	{1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+	{1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+	{1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+	{1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+	{1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+	{1, 4, 4, 4, 4, 4, 4, 4, 4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+	{1, 4, 0, 4, 0, 0, 0, 0, 4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+	{1, 4, 0, 0, 0, 0, 5, 0, 4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+	{1, 4, 0, 4, 0, 0, 0, 0, 4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+	{1, 4, 0, 4, 4, 4, 4, 4, 4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+	{1, 4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+	{1, 4, 4, 4, 4, 4, 4, 4, 4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+	{1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1}};
+}
+
 int			main(void)
 {
-	char	map[MAP_H][MAP_W] =
-	{
-		{1,1,1,1,1,1,1,1,1,1,1,0,0,1,1,1,1,1,1,1,1,1,1,1},
-		{1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
-		{1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
-		{1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
-		{1,0,0,0,0,0,2,2,2,2,2,0,0,0,0,3,0,3,0,3,0,0,0,1},
-		{1,0,0,0,0,0,2,0,0,0,2,0,0,0,0,0,0,0,0,0,0,0,0,1},
-		{1,0,0,0,0,0,2,0,0,0,2,0,0,0,0,3,0,0,0,3,0,0,0,1},
-		{1,0,0,0,0,0,2,0,0,0,2,0,0,0,0,0,0,0,0,0,0,0,0,1},
-		{1,0,0,0,0,0,2,2,0,2,2,0,0,0,0,3,0,3,0,3,0,0,0,1},
-		{1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
-		{1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
-		{1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
-		{1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
-		{1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
-		{1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
-		{1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
-		{1,4,4,4,4,4,4,4,4,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
-		{1,4,0,4,0,0,0,0,4,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
-		{1,4,0,0,0,0,5,0,4,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
-		{1,4,0,4,0,0,0,0,4,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
-		{1,4,0,4,4,4,4,4,4,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
-		{1,4,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
-		{1,4,4,4,4,4,4,4,4,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
-		{1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1}
-	};
-
+	char	map[MAP_H][MAP_W];
 	t_cl	cl;
 	t_sdl	sdl;
 
 	ft_bzero(&cl, sizeof(cl));
 	ft_bzero(&sdl, sizeof(sdl));
 
+	map_init(map);
 	cl_init(&cl, 1, (t_arg){map, sizeof(map), CL_MEM_READ_ONLY});
 	sdl_init(&sdl, PROGRAM_NAME);
 
