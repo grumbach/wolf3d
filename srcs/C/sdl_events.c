7026fd2acd483e345243443be010cdf6f0b1f2f7
Antonin GAVREL <angavrel@e3r10p15.42.fr>
2017-11-18 22:02:04 +0100

    [ADD] Straffing hook event and some opti

--- srcs/C/sdl_events.c
+++ srcs/C/sdl_events.c
@@ -6,14 +6,14 @@
 /*   By: agrumbac <agrumbac@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/07/12 12:49:22 by agrumbac          #+#    #+#             */
-/*   Updated: 2017/11/05 16:21:58 by agrumbac         ###   ########.fr       */
+/*   Updated: 2017/11/18 22:01:14 by angavrel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "easy_sdl.h"
 #include "wolf3d.h"
 
-static void			turn_cam(t_cam *cam, const float speed)
+static int			turn_cam(t_cam *cam, const float speed)
 {
 	cam->direction = (t_vector)
 	{
@@ -25,40 +25,45 @@ static void			turn_cam(t_cam *cam, const float speed)
 		cam->plane.x * cos(speed) - cam->plane.y * sin(speed),
 		cam->plane.x * sin(speed) + cam->plane.y * cos(speed)
 	};
+	return (EVENT_UPDATE);
 }
 
-static void			move_cam(t_cam *cam, const float speed)
+static int			move_cam(t_cam *cam, const float speed, const int a)
 {
 	t_vector		new;
 
 	new = (t_vector)
 	{
-		cam->origin.x + cam->direction.x * speed,
-		cam->origin.y + cam->direction.y * speed
+		cam->origin.x + (a * cam->direction.x + (1 - a) * cam->plane.x) * speed,
+		cam->origin.y + (a * cam->direction.y + (1 - a) * cam->plane.y) * speed
 	};
 	if (new.x > MAP_SIZE || new.x < 0)
 		new.x = cam->origin.x;
 	if (new.y > MAP_SIZE || new.y < 0)
 		new.y = cam->origin.y;
 	cam->origin = (t_vector){new.x, new.y};
+	return (EVENT_UPDATE);
 }
 
 static int			sdl_keyboard(t_cam *cam)
 {
 	const Uint8		*state = SDL_GetKeyboardState(NULL);
+	int				event;
 
+	event = EVENT_IDLE;
 	if (state[SDL_SCANCODE_W])
-		move_cam(cam, MOVE_SPEED);
-	if (state[SDL_SCANCODE_S])
-		move_cam(cam, -MOVE_SPEED);
+		event = move_cam(cam, MOVE_SPEED, 1);
+	else if (state[SDL_SCANCODE_S])
+		event = move_cam(cam, -MOVE_SPEED, 1);
 	if (state[SDL_SCANCODE_A])
-		turn_cam(cam, TURN_SPEED);
-	if (state[SDL_SCANCODE_D])
-		turn_cam(cam, -TURN_SPEED);
-	if (state[SDL_SCANCODE_S] || state[SDL_SCANCODE_W] || \
-		state[SDL_SCANCODE_A] || state[SDL_SCANCODE_D])
-		return (EVENT_UPDATE);
-	return (EVENT_IDLE);
+		event = turn_cam(cam, TURN_SPEED);
+	else if (state[SDL_SCANCODE_D])
+		event = turn_cam(cam, -TURN_SPEED);
+	if (state[SDL_SCANCODE_Q])
+		event = move_cam(cam, -MOVE_SPEED / 2, 0);
+	else if (state[SDL_SCANCODE_E])
+		event = move_cam(cam, MOVE_SPEED / 2, 0);
+	return (event);
 }
 
 static int			sdl_mouse(t_sdl *sdl, t_cam *cam)
@@ -76,7 +81,7 @@ int					sdl_events(t_sdl *sdl, t_cam *cam)
 {
 	t_xy			window_size;
 
-	if (SDL_PollEvent(&sdl->event))
+	while (SDL_PollEvent(&sdl->event))
 	{
 		if ((sdl->event.type == SDL_WINDOWEVENT && \
 			sdl->event.window.type == SDL_WINDOWEVENT_CLOSE) || \
